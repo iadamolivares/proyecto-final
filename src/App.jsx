5e979f4c5473d4d9f2d9d830c1504d755c5a82d2
iadamolivares <ignacioadam.olivares@gmail.com>
2026-08-04 16:07:17 -0400

    add: Letters counter

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,73 @@ const App = () => {
   const [text, setText] = useState(
     "Esto es un texto de prueba, puedes modificarlo.",
   );
-  const [excludeSpaces, setExcludeSpaces] = useState(false)
+  const [excludeSpaces, setExcludeSpaces] = useState(false);
 
-  const [limitCharacter, setLimitCharacter] = useState(false)
+  const [limitCharacter, setLimitCharacter] = useState(false);
 
-  const [limitValue,setLimitValue] = useState (10)
+  const [limitValue, setLimitValue] = useState(10);
 
-  const characters = excludeSpaces ? text.replace(/\s/g,"").length : text.length
+  const [showAll, setShowAll] = useState(false)
 
-  const words = text.trim() === "" ? 0 : text.trim().split(/\s+/).length
+  const characters = excludeSpaces
+    ? text.replace(/\s/g, "").length
+    : text.length;
 
-  const sentences = text.trim() === "" ? 0 : text.split(/[.!,?]+/).filter(sentence => sentence.trim() !== "").length
+  const words = text.trim() === "" ? 0 : text.trim().split(/\s+/).length;
+
+  const sentences =
+    text.trim() === ""
+      ? 0
+      : text.split(/[.!?]+/).filter((sentence) => sentence.trim() !== "")
+          .length;
+
+  const readingTime = Math.ceil(words / 200);
 
   const handleChangeTextarea = (e) => {
-    const value = e.target.value
+    const value = e.target.value;
     if (limitCharacter) {
       if (value.length <= limitValue) {
-        setText(value)
+        setText(value);
       }
     } else {
-      setText(value)
+      setText(value);
     }
-  }
+  };
 
   const handleChangeInputLimit = () => {
-    setLimitCharacter (!limitCharacter)
-    const newText = text.slice(0, limitValue)
-  setText(newText)
-  }
+    setLimitCharacter(!limitCharacter);
+    const newText = text.slice(0, limitValue);
+    setText(newText);
+  };
+
+  const cleanText = text.toLowerCase().replace(/[^a-záéíóú0-9ñü]/g, "")
+  const total = cleanText.length
+
+  const dictionaryLetters = {}
+
+  cleanText.split("").forEach(letter => {
+    dictionaryLetters[letter] = (dictionaryLetters[letter] || 0) + 1
+  })
+
+  const letters = Object.entries(dictionaryLetters).map(dataLetter => {
+    const letter = dataLetter[0]
+    const amaountLetter = dataLetter[1]
+
+    const infoToRenderLetter = {
+      letterName: letter,
+      amount: amaountLetter,
+      percentage: (amaountLetter/ total) * 100
+    }
+
+    return infoToRenderLetter
+  })
+
+  const sortLetters = letters.sort((a, b) => b.amount - a.amount)
+
+  const visibleLetters = showAll ? sortLetters : sortLetters.slice(0, 5)
+   
+
+  
 
   return (
     <main>
@@ -47,32 +86,47 @@ const App = () => {
       ></textarea>
       <div>
         <label>
-        <input 
-        type="checkbox" 
-        checked={excludeSpaces}
-        onChange={() => setExcludeSpaces(!excludeSpaces)}
-        />
-        Exclude Spaces
-        </label>
-           <label>
-        <input 
-        type="checkbox" 
-        checked={limitCharacter}
-        onChange={handleChangeInputLimit}
-        />
-        Set Character Limit
-        </label>
-        {
-          limitCharacter && 
-          <input type="number" 
-          value={limitValue}
-          onChange={(e) => setLimitValue(e.target.value)}
+          <input
+            type="checkbox"
+            checked={excludeSpaces}
+            onChange={() => setExcludeSpaces(!excludeSpaces)}
           />
-        }
+          Excluir espacios
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            checked={limitCharacter}
+            onChange={handleChangeInputLimit}
+          />
+          Limite de caracteres
+        </label>
+        {limitCharacter && (
+          <input
+            type="number"
+            value={limitValue}
+            onChange={(e) => setLimitValue(e.target.value)}
+          />
+        )}
       </div>
       <p>Cantidad de caracteres: {characters}</p>
       <p>Cantidad de palabras: {words}</p>
       <p>Cantidad de oraciones: {sentences}</p>
+      <p>Tiempo aprox. de lectura: ~{readingTime} min</p>
+      <section>
+        <h2>Cantidad de letras</h2>
+        <button onClick={() => setShowAll(!showAll)}>{showAll ? "Ver menos 🔼" : "Ver todos 🔽"}</button>
+        <article>
+          {
+            visibleLetters.map(letter => 
+            <div key={letter.letterName}>
+            <span>{letter.letterName.toLocaleUpperCase()}</span>
+            <meter min="0" max="100" value={letter.percentage}></meter>
+            <span>{letter.amount}({letter.percentage.toFixed(1)})%</span>
+          </div>)
+          }
+        </article>
+      </section>
     </main>
   );
 };
