3dacc7ddbd7aa56961a3d079f221984a7501593b
iadamolivares <ignacioadam.olivares@gmail.com>
2026-08-04 10:08:06 -0400

    add: checkbox exclude spaces

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
-import { Header } from "./components/Header.jsx"
-function App() {
+import { useState } from "react";
+import { Header } from "./components/Header.jsx";
+const App = () => {
+  const [text, setText] = useState(
+    "Esto es un texto de prueba, puedes modificarlo.",
+  );
+  const [excludeSpaces, setExcludeSpaces] = useState(false)
+
+  const characters = excludeSpaces ? text.replace(/\s/g,"").length : text.length
+
+
   return (
     <main>
-    <Header/>
+      <Header />
+      <h2>
+        Analyze your text <br />
+        in real-time.
+      </h2>
+      <textarea
+        placeholder="Escribe tu texto..."
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+      ></textarea>
+      <div>
+        <label>
+        <input 
+        type="checkbox" 
+        checked={excludeSpaces}
+        onChange={() => setExcludeSpaces(!excludeSpaces)}
+        />
+        Exclude Spaces
+        </label>
+      </div>
+      <p>Cantidad de caracteres: {characters}</p>
     </main>
-  )     
-}
+  );
+};
 
-export {App}
+export { App };
