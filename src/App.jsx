fe116345fe36b31e0f43be0d2ad8bf6e858808ee
iadamolivares <ignacioadam.olivares@gmail.com>
2026-08-04 12:02:52 -0400

    add: Words counter, Sentences counter

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,32 @@ const App = () => {
   );
   const [excludeSpaces, setExcludeSpaces] = useState(false)
 
+  const [limitCharacter, setLimitCharacter] = useState(false)
+
+  const [limitValue,setLimitValue] = useState (10)
+
   const characters = excludeSpaces ? text.replace(/\s/g,"").length : text.length
 
+  const words = text.trim() === "" ? 0 : text.trim().split(/\s+/).length
+
+  const sentences = text.trim() === "" ? 0 : text.split(/[.!,?]+/).filter(sentence => sentence.trim() !== "").length
+
+  const handleChangeTextarea = (e) => {
+    const value = e.target.value
+    if (limitCharacter) {
+      if (value.length <= limitValue) {
+        setText(value)
+      }
+    } else {
+      setText(value)
+    }
+  }
+
+  const handleChangeInputLimit = () => {
+    setLimitCharacter (!limitCharacter)
+    const newText = text.slice(0, limitValue)
+  setText(newText)
+  }
 
   return (
     <main>
@@ -18,7 +42,7 @@ const App = () => {
       </h2>
       <textarea
         placeholder="Escribe tu texto..."
-        onChange={(e) => setText(e.target.value)}
+        onChange={handleChangeTextarea}
         value={text}
       ></textarea>
       <div>
@@ -30,8 +54,25 @@ const App = () => {
         />
         Exclude Spaces
         </label>
+           <label>
+        <input 
+        type="checkbox" 
+        checked={limitCharacter}
+        onChange={handleChangeInputLimit}
+        />
+        Set Character Limit
+        </label>
+        {
+          limitCharacter && 
+          <input type="number" 
+          value={limitValue}
+          onChange={(e) => setLimitValue(e.target.value)}
+          />
+        }
       </div>
       <p>Cantidad de caracteres: {characters}</p>
+      <p>Cantidad de palabras: {words}</p>
+      <p>Cantidad de oraciones: {sentences}</p>
     </main>
   );
 };
